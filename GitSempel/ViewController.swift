e3dbc0e35434272794a4bb9b92ee0bf020c05fcd
ivanprybo <ivan.prybo@gmail.com>
2018-12-03 20:00:42 +0300

    Initial Commit

---
+++ GitSempel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitSempel
+//
+//  Created by Ivan on 12/3/18.
+//  Copyright © 2018 Ivan Prybolovetz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
